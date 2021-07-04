61bb690cdbf363129a2ea41ca5faf479b7fa6378
knnthgms <knnthgms@gmail.com>
2021-07-04 21:09:38 +0530

    detecting device type

---
+++ src/utils/index.jsx
@@ -0,0 +1,9 @@
+const UTILS = {
+  isMobile: () => {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+  },
+};
+
+export default UTILS;
